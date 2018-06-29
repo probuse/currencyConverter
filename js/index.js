8227dc1ae1e5ce7a89259e132d84b96dd61afce3
probuse <nprobuse@gmail.com>
2018-06-29 11:45:16 +0300

    Fix Promise Not Working error

--- js/index.js
+++ js/index.js
@@ -1,36 +1,47 @@
-function startConversion(){
-    const fromCurrency = document.getElementById('fromCurrency').value;
-    const fromAmount = document.getElementById('fromAmount').value;
-    const toCurrency = document.getElementById('toCurrency').value;
-    
-    const requestParams = {
-        fromCurrency: fromCurrency,
-        fromAmount: fromAmount,
-        toCurrency: toCurrency
-    };
-
-    // callAPI(requestParams); 
-    getCurrencies()
-}
-
-function getCurrencies(){
-    fetch('https://free.currencyconverterapi.com/api/v5/currencies')
-        .then(response => {
-            return response.json();
-        }).then(data => {
+window.addEventListener('load', () => {
+    // remember to consult the cache for offline functionality.
+    fetch('https://free.currencyconverterapi.com/api/v5/currencies').then(response => {       
+        return response.json();   
+    }).then(data => {
+        for(const currency of Object.values(data.results)){
+            /* populate option fields */
+            let currencyOption = document.createElement('option');
             let fromSelect = document.getElementById('fromCurrency');
-            for(const currency of Object.keys(data.results)){
-                // console.log(currency);
-                let currencyOption = fillCurrencyDropdownMenu(currency);
-                fromSelect.appendChild(currencyOption);
-            }
-        }).catch(error => {
-            console.log(error);
-        });
-}
+            currencyOption.textContent = currency.currencyName;
+            currencyOption.setAttribute("id", currency.id);
+            fromSelect.appendChild(currencyOption);
+        }
 
-function fillCurrencyDropdownMenu(currency){
-    let currencyOption = document.createElement('option');
-    currencyOption.textContent = currency;
-    return currencyOption;
+        for(const currency of Object.values(data.results)){
+            /* populate option fields */
+            let currencyOption = document.createElement('option');
+            let toSelect = document.getElementById('toCurrency');
+            currencyOption.textContent = currency.currencyName;
+            currencyOption.setAttribute("id", currency.id);
+            toSelect.appendChild(currencyOption);
+        }
+    });
+});
+
+document.getElementById('submitCurrency').addEventListener('click', () => {
+    startConversion();
+});
+
+function startConversion(){
+    const fromSelector = document.getElementById('fromCurrency');
+    const toSelector = document.getElementById('toCurrency');
+    const fromAmount = document.getElementById('fromAmount').value;
+    const fromCurrency = fromSelector.value;
+    const toCurrency = toSelector.value;
+    const fromId = encodeURIComponent(fromSelector[fromSelector.selectedIndex].id);
+    const toId = encodeURIComponent(toSelector[toSelector.selectedIndex].id);
+    
+    let query = `${fromId}_${toId}`
+    fetch(`https://free.currencyconverterapi.com/api/v5/convert?q={${query}}`).then(response => {
+        console.log(response.json());
+        console.log('We resolved');
+    }).catch(error => {
+        console.log(error);
+        console.log('We resolved');
+    });
 }
