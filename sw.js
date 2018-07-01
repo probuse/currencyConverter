370df22c458f36dc71afa881a87a60630d4d51a0
probuse <nprobuse@gmail.com>
2018-07-01 05:04:46 +0300

    remove pathname to root

--- sw.js
+++ sw.js
@@ -14,13 +14,11 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
     const requestUrl = new URL(event.request.url);
     if (requestUrl.origin === location.origin){
-        if(requestUrl.pathname === '/'){
             event.respondWith(
                 caches.match(event.request).then((response) => {
                     return response || fetch(event.request);
                 })
             ); 
-        }
     }
     
 });
